2720c8b339d95c73a6636e026c5eaa9773f0ae67
Vincent Jerome Saddi <vinjer55@gmail.com>
2024-10-14 00:44:36 +0800

    Added Database

---
+++ src/db/database.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+
+import os
+
+load_dotenv()
+
+engine = create_engine(os.getenv("URL_DATABASE"))
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
